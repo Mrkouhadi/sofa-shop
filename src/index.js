30b4fda79b0830a5bf33d766ca51e69f50c0fd76
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-04-14 05:56:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
+const root =  document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
       <BrowserRouter>
             <App />
       </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+ root
 );
